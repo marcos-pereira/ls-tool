06e5882a881b297be1865caee55414530ad708c1
Marcos Pereira <marcos.si.pereira@gmail.com>
2026-06-09 19:55:34 -0300

    Implement directory opening and error handling in ls tool

--- ls.c
+++ ls.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+// Use: opendir
+#include <dirent.h>
+
 int main(int argc, char *argv[]) {
     const char *path = (argc > 1) ? argv[1] : ".";
-    printf("%s \n", path);
+
+    // Open the directory specified by the path
+    DIR *dir = opendir(path);
+    if (!dir) {
+        // If opendir fails, print an error message 
+        // and return a non-zero exit code
+        perror("opendir");
+        return 1;
+    }
+
     return 0;
 }

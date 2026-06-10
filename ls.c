ea9f3e403185be41e0941ea995672ba1e712f34a
Marcos Pereira <marcos.si.pereira@gmail.com>
2026-06-09 22:18:18 -0300

    Refactor command-line option parsing and improve path determination in ls tool

--- ls.c
+++ ls.c
@@ -3,8 +3,34 @@
 // Use: opendir, readdir
 #include <dirent.h>
 
+// Use: 
+#include <unistd.h>
+
+int show_all = 0;
+
 int main(int argc, char *argv[]) {
-    const char *path = (argc > 1) ? argv[1] : ".";
+    int opt;
+
+    // Parse command-line options using getopt. 
+    // The only option we support is '-a'
+    while ((opt = getopt(argc, argv, "a")) != -1) {
+        switch (opt) {
+            case 'a':
+                show_all = 1;
+                break;
+            default:
+                // If an unknown option is provided, 
+                // print usage information and exit 
+                // with an error code
+                fprintf(stderr, "Usage: %s [-a] [path]\n", argv[0]);
+                return 1;
+        }
+    }
+
+    // Determine the path to list. If a path is provided as a
+    // command-line argument, use it; otherwise, default to 
+    // the current directory
+    const char *path = (optind < argc) ? argv[optind] : "."; 
 
     // Open the directory specified by the path
     DIR *dir = opendir(path);
@@ -21,7 +47,10 @@ int main(int argc, char *argv[]) {
     // Loop through the directory entries until there 
     // are no more entries to read
     while ((entry = readdir(dir)) != NULL) {
-        if(entry->d_name[0] == '.') {
+        // If the '-a' option is not set and the entry's name
+        // starts with '.', skip it 
+        // (this means it's a hidden file or directory)
+        if(!show_all && entry->d_name[0] == '.') {
             // Skip hidden files and directories 
             // (those starting with '.')
             continue;
